c4a176e647ea66af00ef40deb8e38b7450224354
ratakondasree <sreehari.ratakonda@gmail.com>
2025-04-13 13:16:43 +0530

    adding templates

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
 from dbconnection import create_db_connection
+from flask import Flask, render_template, request
 
-uname = "sreehari"
-pwd = "Rsri@Nov1983"
+app=Flask(__name__)
 
-try:
-    engine=create_db_connection(username=uname,password=pwd)
-    engine.connect()
-    print("✅ SQLAlchemy connected successfully!")
-except Exception as e:
-    print("❌ SQLAlchemy connection failed:", e)
+@app.route("/")
+def welcome():
+    return "This is welcome page"
+@app.route("/form",methods=["GET","POST"])
+def form():
+    try:
+        if request.method=="POST":
+            engine=create_db_connection(username=request.form['login'],password=request.form['password'])
+            engine.connect()
+            return render_template('query.html')
+        return render_template('form.html')
+    except Exception as e:
+        return "<html><h1>❌ SQLAlchemy connection failed:</h1></html>"
+@app.route("/query",methods=["GET","POST"])
+def query():
+    try:
+        return render_template('query.html')
+    except Exception as e:
+        return "<html><h1>❌ SQLAlchemy connection failed:</h1></html>"
+
+if __name__=="__main__":
+    app.run(debug=True)
+
+
+
+
